3628f67fa5c9b55d3b8c0043eded94a6b561adc2
Migersa <105946940+Migersa@users.noreply.github.com>
2022-11-17 23:34:17 +0100

    add

---
+++ ATM machine/DTO/WithdrawDTO/WithdrawAdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.WithdrawDTO
+{
+   public class WithdrawAdd
+    {
+
+     
+        public Guid CardId { get; set; }
+        public double? WithdrawValue { get; set; }
+
+    }
+}
---
+++ ATM machine/DTO/WithdrawDTO/WithdrawDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.WithdrawDTO
+{
+    public class WithdrawDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime DateofWithdraw { get; set; }
+        public Guid CardId { get; set; }
+        public double? WithdrawValue { get; set; }
+    }
+}
